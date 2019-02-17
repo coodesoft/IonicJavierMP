df07e96880b3ae8189267fb1a1cac004b27136ba
lucho <lucho.2012.tandil@gmail.com>
2019-02-16 23:17:28 -0300

    [DEV] Se agrega el provider vacío, y el enlace de menu de la vista también vacía

---
+++ src/pages/mercado-pago/nuevo-pago/nuevo-pago.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { NuevoPagoPage } from './nuevo-pago';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    IonicPageModule.forChild(NuevoPagoPage),
+  ],
+})
+export class NuevoPagoPageModule {}
